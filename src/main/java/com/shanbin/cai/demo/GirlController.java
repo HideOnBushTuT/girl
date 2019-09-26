3bf73de466245bf46cd1a75ac849e67b66cb895d
shanbin cai <shanbin.cai@gmail.com>
2019-09-26 19:03:00 +0800

    use mysql 2

---
+++ src/main/java/com/shanbin/cai/demo/GirlController.java
@@ -0,0 +1,48 @@
+package com.shanbin.cai.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GirlController {
+    @Autowired
+    private GirlRepo girlRepo;
+
+    @GetMapping(value = "/girls")
+    public List<Girl> girlList() {
+        return girlRepo.findAll();
+    }
+
+    @PostMapping(value = "/girls")
+    public Girl girlAdd(@RequestParam("cupSize") String cupSize, @RequestParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setAge(age);
+        girl.setCpuSize(cupSize);
+        return girlRepo.save(girl);
+    }
+
+    @GetMapping(value = "/girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id) {
+        return  girlRepo.findById(id).get();
+    }
+
+    @PutMapping(value = "/girls/{id}")
+    public Girl girlUpdate(@PathVariable("id") Integer id,
+                           @RequestParam("cupSize") String cupSize,
+                           @RequestParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setCpuSize(cupSize);
+        girl.setAge(age);
+
+        return girlRepo.save(girl);
+    }
+
+    @DeleteMapping(value = "/girls/{id}")
+    public void girlDelete(@PathVariable("id") Integer id) {
+        girlRepo.deleteById(id);
+    }
+
+}
---
+++ src/main/java/com/shanbin/cai/demo/GirlRepo.java
@@ -0,0 +1,7 @@
+package com.shanbin.cai.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GirlRepo extends JpaRepository<Girl, Integer> {
+
+}
