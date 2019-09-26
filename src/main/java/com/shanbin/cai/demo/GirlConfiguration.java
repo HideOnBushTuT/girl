87ea239f84bde4506a782b5a1714069eda475546
shanbin cai <shanbin.cai@gmail.com>
2019-09-26 14:54:05 +0800

    @Value & @Component & @configurationProperties

---
+++ src/main/java/com/shanbin/cai/demo/GirlConfiguration.java
@@ -0,0 +1,29 @@
+package com.shanbin.cai.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlConfiguration {
+    private String cupSize;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    private Integer age;
+
+}
